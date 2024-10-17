10a2d18d4564ffde20a9ff85c0826151335e3eab
Alireza Asadi <a.asadi@easygo.io>
2024-10-17 14:23:58 +1100

    fix(android): casing and pip parent view issue

--- ios/IvsPlayerViewManager.swift
+++ ios/IvsPlayerViewManager.swift
@@ -352,19 +352,25 @@ public class IvsPlayerViewManager: RCTViewManager, AVPictureInPictureControllerD
     
     func handleNewAirPlaySource() {
         print("ReactNativeIVSPlayer AirPlay is active")
-        if (self.player.path != nil) {
-            self.airplayButton.removeFromSuperview() // try to hide the airplay selector
-            self.playerView.player?.pause()
-            createAvPlayer(url: self.player.path!)
-            avPlayer?.play()
-            // set PLAYING after 1 sec
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-                self.notifyListeners("onState", data: ["state": "PLAYING"])
-            }
-            // send to listner
-            isCastActive = true
-            self.notifyListeners("onCastStatus", data: ["isActive": true])
+        guard let playerPathUrl = self.player.path else {
+            // Handle the case where player.path is nil, e.g., log an error or return early
+            print("player.path is nil")
+            return
         }
+
+        self.airplayButton.removeFromSuperview() // try to hide the airplay selector
+        self.playerView.player?.pause()
+        createAvPlayer(url: playerPathUrl)
+        avPlayer?.play()
+
+        // set PLAYING after 1 sec
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+            self.notifyListeners("onState", data: ["state": "PLAYING"])
+        }
+
+        // send to listener
+        isCastActive = true
+        self.notifyListeners("onCastStatus", data: ["isActive": true])
     }
     
     func removeAvPlayer() {
